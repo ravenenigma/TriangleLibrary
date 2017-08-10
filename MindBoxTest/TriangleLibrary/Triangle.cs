63239794a2ea7215744f2005768e581ffe7c0f93
ravenenigma <nazarovrb@ipcpi.ru>
2017-08-10 09:18:18 +0700

    Библиотека
    Напишите тесты на код (и сам код), который вычисляет площадь прямоугольного треугольника, принимая на вход длину всех трех его сторон. Код будет поставляться внешним клиентам в составе библиотеки.

---
+++ MindBoxTest/TriangleLibrary/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TriangleLibrary
+{
+    public class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            //проверка на кооректность заданных сторон треугольника
+            if ((a <= 0) || (b <= 0) || (c <= 0))
+            {
+                throw new ArgumentOutOfRangeException("The triangle sides are incorrectly set!");
+            }
+            //Проверка условия неравенства треугольника
+            if (!((a + b >= c) && (a + c >= b) && (b + c >= a)))
+            {
+                throw new ArgumentOutOfRangeException("The condition of inequality of a triangle is satisfied!");
+            }
+
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        /// <summary>
+        /// Вычесление площадь треугольника по 3 сторонам
+        /// по Формуле Герона S=sqrt(p*(p-a)*(p-b)*(p-c))
+        /// </summary>
+        /// <returns>площадь треугольника по 3 сторонам</returns>
+        public double Area()
+        {
+            double p = (A + B + C) / 2;     //Полупериметр треугольника
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));      //Формула Герона для вычесления площади треугольника
+        }
+    }
+
+    /// <summary>
+    /// Проверка прямоугольного треугольника по теореме Пифагора
+    /// </summary>
+    public class RightTriangle : Triangle
+    {
+        public RightTriangle(double a, double b, double c) : base(a, b, c)
+        {
+            if (Math.Abs(a * a + b * b - c * c) > 1E-6)     //теорема Пифагора
+            {
+                throw new ArgumentOutOfRangeException("Isn't a right triangle!");
+            }
+        }
+    }
+}
---
+++ MindBoxTest/TriangleUnitTest/TriangleTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleLibrary;
+
+namespace TriangleUnitTest
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        /// <summary>
+        /// проверка на кооректность заданных сторон треугольника
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void WrongSideLengthTest()
+        {
+            Triangle t = new Triangle(1, 2, 0);
+        }
+
+        /// <summary>
+        /// Проверка условия неравенства треугольника
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleInequalityTest()
+        {
+            Triangle t = new Triangle(2, 3, 6);
+        }
+
+        /// <summary>
+        /// Вычесление площадь треугольника
+        /// </summary>
+        [TestMethod]
+        public void AreaTest()
+        {
+            Triangle t = new Triangle(3, 4, 5);
+
+            const double expected = 3 * 4 / 2;
+            double area = t.Area();
+
+            Assert.AreEqual(expected, area, 1E-3);
+        }
+    }
+
+    /// <summary>
+    /// Проверка прямоугольного треугольника
+    /// </summary>
+    [TestClass]
+    public class RightTriangleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NotRightTriangleTest()
+        {
+            RightTriangle t = new RightTriangle(1, 2, 3);
+        }
+
+        [TestMethod]
+        public void CorrectRightTriangleTest()
+        {
+            RightTriangle t = new RightTriangle(3, 4, 5);
+        }
+    }
+}
